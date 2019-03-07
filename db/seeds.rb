a8736c3ea85e0239eb4edad309a30b2bbbadeecb
3mpirical <3mpirical10510@gmail.com>
2019-03-07 15:16:51 -0700

    seeds filee created and database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all();
+
+
+User.create(
+    first_name: "Betty",
+    last_name: "White",
+    degree: "Econophysionomy",
+    email: "fake@mail.com",
+    password: "password",
+)
+
+for i in (1..3)
+    User.create(
+        first_name: Faker::Name.first_name(),
+        last_name: Faker::Name.last_name(),
+        degree: Faker::Educator.degree(),
+        email: Faker::Internet.email(),
+        password: "password",
+    )
+end
+
+User.all().each() {|user|
+
+    for i in (1..10)
+        course = Course.create(
+            user_id: user.id,
+            course: Faker::Educator.course_name(),
+        )
+
+        Book.create(
+            course_id: course.id,
+            book: Faker::Book.title(),
+            price: "$#{Faker::Commerce.price(0..500)}",
+        )
+
+        Teacher.create(
+            course_id: course.id,
+            teacher: Faker::GreekPhilosophers.name(),
+        )
+    end
+}
+
+
+puts "_____Database_Seeded_____"
